a607b4ff0ab61f43d019830465b036d06991283b
secondary-smiles <shavkinder@gmail.com>
2022-10-09 10:00:32 -0400

    Add better formatted output to log macro

--- src/output/log.rs
+++ src/output/log.rs
@@ -1,11 +1,11 @@
 #[macro_export]
 macro_rules! log {
     ($msg:expr) => {
-        let binding = format!("{}", $msg.clone());
+        let binding = format!("{:?}", $msg.clone());
         {
             eprintln!("\x1b[1;34mINFO:\x1b[0m");
             for line in binding.lines() {
-                eprintln!("  [{:?}]  {}", std::thread::current().name().unwrap_or("unknown"), line);
+                eprintln!("  \x1b[36m[{:?}]:\x1b[0m  {}", std::thread::current().name().unwrap_or("unknown"), line);
             }
         }
     }
