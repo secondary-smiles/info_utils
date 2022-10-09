0846cab328d4dd8c0e10e7de83acc4c8ab72e609
secondary-smiles <shavkinder@gmail.com>
2022-10-09 01:47:24 -0400

    Add eval_option docs

--- src/eval/eval_option.rs
+++ src/eval/eval_option.rs
@@ -1,5 +1,10 @@
+//!# Eval Option
+//!
+//! `eval()` implemented for the `Option<T>` type
+
 use crate::error;
 
+/// Eval Option trait that enables `eval()` for the `Option<T>` type
 pub trait EvalOption<T> {
     fn eval(self) -> T;
     fn eval_or(self, sub: T) -> T;
@@ -8,6 +13,24 @@ pub trait EvalOption<T> {
 }
 
 impl<T> EvalOption<T> for Option<T> {
+    /// Drop-in for the `unwrap()` function
+    ///
+    /// returns `v` if some; else calls an `error!`
+    ///
+    /// Example
+    /// ```rust
+    /// # use crate::log_utils::prelude::*;
+    /// # fn main() {
+    /// struct Foo {
+    ///      data: Option<i32>,
+    ///  }
+    ///
+    ///  let val: Foo = Foo {
+    ///      data: Some(7)
+    ///  };
+    ///  assert_eq!(val.data.eval(), 7);
+    /// # }
+    /// ```
     fn eval(self) -> T {
         match self {
             Some(v) => v,
@@ -15,6 +38,28 @@ impl<T> EvalOption<T> for Option<T> {
         }
     }
 
+    /// Drop-in for the `unwrap_or()` function
+    ///
+    /// returns `v` if some; else returns `sub`
+    ///
+    /// Example
+    /// ```rust
+    /// # use crate::log_utils::prelude::*;
+    /// # fn main() {
+    ///  struct Foo<'a> {
+    ///     data: Option<&'a str>,
+    ///  }
+    ///
+    ///  let mut val: Foo = Foo {
+    ///     data: Some("info")
+    ///  };
+    ///
+    ///  assert_eq!(val.data.eval_or("data"), "info");
+    ///
+    ///  val.data = None;
+    ///  assert_eq!(val.data.eval_or("data"), "data");
+    /// # }
+    /// ```
     fn eval_or(self, sub: T) -> T {
         match self {
             Some(v) => v,
@@ -22,6 +67,28 @@ impl<T> EvalOption<T> for Option<T> {
         }
     }
 
+    /// Drop-in for the `unwrap_or_default()` function
+    ///
+    /// returns `v` if some; else returns default value for type `T`
+    ///
+    /// Example
+    /// ```rust
+    /// # use crate::log_utils::prelude::*;
+    /// # fn main() {
+    /// struct Foo {
+    ///    data: Option<i32>,
+    /// }
+    ///
+    /// let mut val: Foo = Foo {
+    /// data: Some(7)
+    /// };
+    ///
+    /// assert_eq!(val.data.eval_or_default(), 7);
+    ///
+    /// val.data = None;
+    /// assert_eq!(val.data.eval_or_default(), 0);
+    /// # }
+    /// ```
     fn eval_or_default(self) -> T where T: Default {
         match self {
             Some(v) => v,
@@ -29,6 +96,29 @@ impl<T> EvalOption<T> for Option<T> {
         }
     }
 
+    /// Drop-in for the `unwrap_or_else()` function
+    ///
+    /// returns `v` if some; else returns return value of function `func`
+    ///
+    /// Example
+    /// ```rust
+    /// # use crate::log_utils::prelude::*;
+    /// # fn main() {
+    /// struct Foo {
+    ///     data: Option<i32>,
+    /// }
+    ///
+    /// let mut val: Foo = Foo {
+    ///     data: Some(7)
+    /// };
+    ///
+    /// let bar = 10;
+    /// assert_eq!(val.data.unwrap_or_else(|| 3 * bar), 7);
+    ///
+    /// val.data = None;
+    /// assert_eq!(val.data.unwrap_or_else(|| 3 * bar), 30);
+    /// # }
+    /// ```
     fn eval_or_else<F>(self, func: F) -> T where F: FnOnce() -> T {
         match self {
             Some(v) => v,
