baa85b4565b7bb6ac3128cae8cc8c1e800280855
secondary-smiles <shavkinder@gmail.com>
2022-10-09 00:51:09 -0400

    Add tests for Option type

--- src/result/eval_option.rs
+++ src/result/eval_option.rs
@@ -36,3 +36,69 @@ impl<T> EvalOption<T> for Option<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_eval() {
+        struct Foo {
+            data: Option<i32>
+        }
+
+        let val: Foo = Foo {
+            data: Some(7)
+        };
+        assert_eq!(val.data.eval(), 7);
+    }
+
+    #[test]
+    fn test_eval_or() {
+        struct Foo<'a> {
+            data: Option<&'a str>
+        }
+
+        let mut val: Foo = Foo {
+            data: Some("info")
+        };
+
+        assert_eq!(val.data.eval_or("data"), "info");
+
+        val.data = None;
+        assert_eq!(val.data.eval_or("data"), "data");
+    }
+
+    #[test]
+    fn test_eval_or_default() {
+        struct Foo {
+            data: Option<i32>
+        }
+
+        let mut val: Foo = Foo {
+            data: Some(7)
+        };
+
+        assert_eq!(val.data.eval_or_default(), 7);
+
+        val.data = None;
+        assert_eq!(val.data.eval_or_default(), 0);
+    }
+
+    #[test]
+    fn test_eval_or_else() {
+        struct Foo {
+            data: Option<i32>
+        }
+
+        let mut val: Foo = Foo {
+            data: Some(7)
+        };
+
+        let foo = 10;
+        assert_eq!(val.data.unwrap_or_else(|| 3*foo), 7);
+
+        val.data = None;
+        assert_eq!(val.data.unwrap_or_else(|| 3*foo), 30);
+    }
+}
