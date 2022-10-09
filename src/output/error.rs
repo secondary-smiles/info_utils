696f3ccbadd7a68baefa63256f1aaea02184942e
secondary-smiles <shavkinder@gmail.com>
2022-10-09 12:53:46 -0400

    Rewrite all log macros

--- src/output/error.rs
+++ src/output/error.rs
@@ -1,24 +1,13 @@
 #[macro_export]
 macro_rules! error {
-    ($msg: expr) => {
-        {
-            let binding = format!("{}", $msg.clone());
-            eprintln!("\x1b[1;31mERR:\x1b[0m");
-            for line in binding.lines() {
-                eprintln!("  {}", line);
-            }
-            std::process::exit(1);
-        }
-    };
-    ($msg:expr, $code:expr) => {
-        {
-            let binding = format!("{}", $msg.clone());
-            eprintln!("\x1b[1;31mErr:\x1b[0m");
-            for line in binding.lines() {
-                eprintln!("  {}", line);
-            }
-            std::process::exit($code);
-        }
-    };
+    ($($msg:tt)*) => {{
+        let binding = format!("{}",format_args!($($msg)*));
 
+            eprintln!("\x1b[1;31mERR\x1b[39m");
+            eprintln!("\x1b[36m[{:?}]:\x1b[39m", std::thread::current().name().unwrap_or("<unknown>"));
+            for line in binding.lines() {
+                eprintln!("  {}", line);
+            }
+        std::process::exit(1);
+    }}
 }
--- src/output/log.rs
+++ src/output/log.rs
@@ -1,12 +1,12 @@
 #[macro_export]
 macro_rules! log {
-    ($msg:expr) => {
-        let binding = format!("{:?}", $msg.clone());
-        {
-            eprintln!("\x1b[1;34mINFO:\x1b[0m");
-            for line in binding.lines() {
-                eprintln!("  \x1b[36m[{:?}]:\x1b[0m  {}", std::thread::current().name().unwrap_or("unknown"), line);
-            }
+    ($($msg:tt)*) => {{
+        let binding = format!("{}",format_args!($($msg)*));
+
+        eprintln!("\x1b[1;34mINFO\x1b[39m");
+        eprintln!("\x1b[36m[{:?}]:\x1b[39m", std::thread::current().name().unwrap_or("<unknown>"));
+        for line in binding.lines() {
+            eprintln!("  {}", line);
         }
-    }
+    }}
 }
