4eb8fafbdea18019de460865a1cf25c2b8a3a976
secondary-smiles <shavkinder@gmail.com>
2022-10-09 00:29:08 -0400

    Add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
 pub mod output;
 pub mod result;
+
+pub mod prelude {
+    pub use crate::{log, error, warn};
+
+    pub use crate::result::eval_option::EvalOption;
+    pub use crate::result::eval_result::EvalResult;
+}
