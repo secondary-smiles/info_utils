c60f6a39ec12c95fbb3c626716c341da5059b58b
secondary-smiles <shavkinder@gmail.com>
2022-10-11 00:08:39 -0400

    Add \r escape to all lines in log functions
    - Now plays nicer with the rest of a program when the terminal is in raw-mode

--- src/output/warn.rs
+++ src/output/warn.rs
@@ -3,10 +3,10 @@ macro_rules! warn {
     ($($msg:tt)*) => {{
         let binding = format!("{}",format_args!($($msg)*));
 
-        eprintln!("\x1b[1;33mWARN\x1b[0m");
-        println!("\x1b[36m[{:?}]:\x1b[39m", std::thread::current().name().unwrap_or("<unknown>"));
+        eprintln!("\x1b[1;33mWARN\x1b[0m\r");
+        println!("\x1b[36m[{:?}]:\x1b[39m\r", std::thread::current().name().unwrap_or("<unknown>"));
         for line in binding.lines() {
-            println!("  {}", line);
+            println!("  {}\r", line);
         }
     }}
 }
